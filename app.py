f7b45dd40d33cb4189dfdccb69f473a2f083c69d
Gabriel Rocha <you@example.com>
2019-10-31 09:34:18 -0300

    modificado app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def inicio():
+    return render_template("inicio.html")
+
+@app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route('/page_1')
 def page_1():
